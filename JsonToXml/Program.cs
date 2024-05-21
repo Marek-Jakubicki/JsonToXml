4c3ccd884ae1661dffcde5787d8ce0fb63181f3a
Marek Jakubicki <marek_jakubicki@epam.pl>
2024-05-21 14:18:54 +0200

    changes

--- JsonToXml/Program.cs
+++ JsonToXml/Program.cs
@@ -1,5 +1,5 @@
-﻿using Newtonsoft.Json;
-using System.Xml.Linq;
+﻿using System.Text.Json;
+using System.Xml;
 
 namespace JsonToXml
 {
@@ -28,23 +28,21 @@ namespace JsonToXml
                 try
                 {
                     string jsonContent = File.ReadAllText(jsonFile);
+                    var jsonDocument = JsonDocument.Parse(jsonContent);
 
-                    XDocument xmlDoc;
-                    try
+                    var xml = ConvertJsonToXml(jsonDocument);
+
+                    if(xml != null)
                     {
-                        xmlDoc = JsonConvert.DeserializeXNode(jsonContent);
+                        string outputFilePath = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(jsonFile) + ".xml");
+                        xml.Save(outputFilePath);
+
+                        Console.WriteLine($"Converted '{jsonFile}' to '{outputFilePath}'.");
                     }
-                    catch
+                    else
                     {
-                        xmlDoc = JsonConvert.DeserializeXNode($"{{\"TempRoot\":{jsonContent}}}", "TempRoot");
-                        xmlDoc = new XDocument(xmlDoc.Root.Elements().First());
+                        Console.WriteLine($"No root element found in '{jsonFile}'.");
                     }
-
-                    string outputFilePath = Path.Combine(outputDirectory, Path.GetFileNameWithoutExtension(jsonFile) + ".xml");
-
-                    xmlDoc.Save(outputFilePath);
-
-                    Console.WriteLine($"Converted '{jsonFile}' to '{outputFilePath}'.");
                 }
                 catch(Exception ex)
                 {
@@ -53,6 +51,88 @@ namespace JsonToXml
             }
 
             Console.WriteLine("Conversion process completed.");
+
+
+
+            XmlDocument ConvertJsonToXml(JsonDocument jsonDocument)
+            {
+                XmlDocument xmlDoc = new();
+
+                XmlDeclaration xmlDeclaration = xmlDoc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                xmlDoc.AppendChild(xmlDeclaration);
+
+                JsonElement rootElement = jsonDocument.RootElement;
+                JsonProperty rootProperty = rootElement.EnumerateObject().First();
+                XmlElement xmlRoot = xmlDoc.CreateElement(SanitizeElementName(rootProperty.Name));
+                xmlDoc.AppendChild(xmlRoot);
+
+                ProcessJsonElement(rootProperty.Value, xmlRoot, xmlDoc);
+
+                return xmlDoc;
+            }
+
+            void ProcessJsonElement(JsonElement element, XmlElement parentXmlElement, XmlDocument xmlDoc)
+            {
+                switch(element.ValueKind)
+                {
+                    case JsonValueKind.Object:
+                    foreach(JsonProperty property in element.EnumerateObject())
+                    {
+                        if(property.Name.StartsWith("@xmlns"))
+                        {
+                            string[] parts = property.Name.Split(':');
+                            if(parts.Length > 1)
+                            {
+                                parentXmlElement.SetAttribute("xmlns:" + parts[1], property.Value.GetString());
+                            }
+                            else
+                            {
+                                parentXmlElement.SetAttribute("xmlns", property.Value.GetString());
+                            }
+                        }
+                        else if(property.Name.StartsWith("@"))
+                        {
+                            parentXmlElement.SetAttribute(property.Name.Substring(1), property.Value.GetString());
+                        }
+                        else
+                        {
+                            XmlElement childElement = xmlDoc.CreateElement(SanitizeElementName(property.Name));
+                            parentXmlElement.AppendChild(childElement);
+                            ProcessJsonElement(property.Value, childElement, xmlDoc);
+                        }
+                    }
+                    break;
+                    case JsonValueKind.Array:
+                    foreach(JsonElement arrayElement in element.EnumerateArray())
+                    {
+                        XmlElement itemElement = xmlDoc.CreateElement(parentXmlElement.Name);
+                        parentXmlElement.ParentNode.InsertAfter(itemElement, parentXmlElement);
+                        ProcessJsonElement(arrayElement, itemElement, xmlDoc);
+                    }
+                    parentXmlElement.ParentNode.RemoveChild(parentXmlElement);
+                    break;
+                    case JsonValueKind.String:
+                    case JsonValueKind.Number:
+                    case JsonValueKind.True:
+                    case JsonValueKind.False:
+                    parentXmlElement.InnerText = element.ToString();
+                    break;
+                    case JsonValueKind.Null:
+                    parentXmlElement.InnerText = "null";
+                    break;
+                }
+            }
+
+            string SanitizeElementName(string name)
+            {
+                if(name.StartsWith("_"))
+                    return name.Substring(1);
+
+                return System.Text.RegularExpressions.Regex.Replace(name, @"[^a-zA-Z0-9_\-\.]", "_");
+            }
+
         }
+
+
     }
 }
